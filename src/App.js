3445d1029037e1e4aa9fbec04825618dea82d75d
Decky Coss <deckycoss@gmail.com>
2018-09-05 17:59:43 -0600

    implement relative trigger directions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,58 @@ import './App.css';
 
 const BehaviorEnum = Object.freeze({
     TriggerLeft: 1,
-    TriggerRight: 2,
-    TriggerUp: 3,
+    TriggerUp: 2,
+    TriggerRight: 3,
     TriggerDown: 4,
+    TriggerForward: 5,
+    TriggerBackward: 6,
+    TriggerCounterClockwise: 7,
+    TriggerClockwise: 8,
 })
+
+function relativeDirectionToAbsolute(fromAbsolute, toRelative){
+
+    // assumes BehaviorEnum absolute directions are 1 to 4 in clockwise order
+
+    // if toRelative is actually an absolute direction, return it unchanged
+    if (toRelative < 5 && toRelative > 0) {
+        return toRelative;
+    }
+    // if fromAbsolute is not absolute, return undefined
+    else if (fromAbsolute < 1 || fromAbsolute > 4){
+        return;
+    }
+
+    var result;
+
+    switch (toRelative){
+        case BehaviorEnum.TriggerForward:
+            result = fromAbsolute;
+            break;
+        case BehaviorEnum.TriggerBackward:
+            result = (fromAbsolute + 2) % 4 || 4;
+            break;
+        case BehaviorEnum.TriggerClockwise:
+            result = (fromAbsolute + 1) % 4 || 4;
+            break;
+        case BehaviorEnum.TriggerCounterClockwise:
+            result = (fromAbsolute + 3) % 4 || 4;
+            break;
+    }
+
+    return result;
+}
 
 function newCellData(){
     return {
         playing: false,
         behaviors: {
-            onTrigger: [
-                Math.ceil(Math.random() * BehaviorEnum.TriggerDown)
+            onTriggerByPress: [
+                // Math.ceil(Math.random() * BehaviorEnum.TriggerDown)
+                BehaviorEnum.TriggerRight
+            ],
+            onTriggerByCell: [
+                BehaviorEnum.TriggerClockwise
             ]
         }
     };
@@ -49,37 +90,50 @@ class Matrix extends Component {
         this.handleCellPress = this.handleCellPress.bind(this);
         this.playStep = this.playStep.bind(this);
     }
-    
+
     handleCellPress(cell, e){
         let index = (cell.props.row * this.props.numCols) + cell.props.col;
 
         this.setState((prevState) => {
             return update(prevState, {
-                queue: {$push: [index]}
+                queue: {$push: [{triggeredByIndex: -1, index}]}
             });
         });
         // this.triggerCell(index, true);
     }
-    
+
     playStep(){
         this.setState((prevState) => {
             let newState = {};
             let newQueue = [];
-            
-            for (let k in prevState.queue){
-                let index = prevState.queue[k];
-                let prevCellState = prevState[index];
 
-                for (let i in prevCellState.behaviors.onTrigger){
-                    let behavior = prevCellState.behaviors.onTrigger[i];
+            for (let k in prevState.queue){
+                let index = prevState.queue[k].index;
+                let triggeredByIndex = prevState.queue[k].triggeredByIndex;
+
+                let prevCellState = prevState[index];
+                let behaviors = prevCellState.behaviors[(triggeredByIndex < 0) ? "onTriggerByPress" : "onTriggerByCell"];
+
+                for (let i in behaviors){
                     let cellToQueue = -1;
-                    
-                    switch (behavior){
+
+                    // if triggered by a press, relative directions are
+                    // meaningless, so use behavior as is. if triggered by
+                    // another cell, and the behavior to respond with is a
+                    // relative direction, convert that to an absolute direction
+
+                    let absoluteBehavior = (triggeredByIndex < 0) ? behaviors[i] :
+                                           relativeDirectionToAbsolute(prevState.queue[k].triggeredByDirection, behaviors[i]);
+
+                    // determine which neighbours, if any, should be triggered
+                    // on the next cycle
+
+                    switch (absoluteBehavior){
                         case BehaviorEnum.TriggerLeft:
                             cellToQueue = (index % this.props.numCols) ? index - 1 : cellToQueue;
                             break;
                         case BehaviorEnum.TriggerRight:
-                            cellToQueue = (index % this.props.numCols !== this.props.numCols - 1) ? index + 1 : cellToQueue; 
+                            cellToQueue = (index % this.props.numCols !== this.props.numCols - 1) ? index + 1 : cellToQueue;
                             break;
                         case BehaviorEnum.TriggerUp:
                             cellToQueue = (index >= this.props.numCols) ? index - this.props.numCols : cellToQueue;
@@ -88,16 +142,16 @@ class Matrix extends Component {
                             cellToQueue = (index < (this.props.numRows - 1) * this.props.numCols) ? index + this.props.numCols : cellToQueue;
                             break;
                     }
-                    
+
                     if (cellToQueue >= 0){
-                        newQueue.push(cellToQueue);
+                        newQueue.push({index: cellToQueue, triggeredByIndex: index, triggeredByDirection: absoluteBehavior});
                     }
                 }
                 newState[index] = update(prevCellState, {
                     playing: {$apply: (x) => !x}
                 });
             }
-            
+
             newState.queue = newQueue;
             return newState;
         });
